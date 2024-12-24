ad768e3dbc0b7322a020de83bb436e6999e368fc
Henry Bui <henry.h.bui05@gmail.com>
2024-12-24 18:21:20 -0500

    refactor IntegerString

--- src/main/java/com/ra4king/circuitsim/gui/properties/IntegerString.java
+++ src/main/java/com/ra4king/circuitsim/gui/properties/IntegerString.java
@@ -1,8 +1,12 @@
 package com.ra4king.circuitsim.gui.properties;
 
+import java.util.Objects;
+
 import com.ra4king.circuitsim.simulator.SimulationException;
 
 /**
+ * A wrapper around integers which can be constructed from strings of different bases.
+ * 
  * @author Roi Atalla
  */
 public final class IntegerString {
@@ -10,40 +14,88 @@ public final class IntegerString {
 	private final int value;
 	private final int base;
 	
-	public IntegerString(String value) {
-		String valueToParse;
-		if (value.startsWith("0x")) {
-			base = 16;
-			valueString = value;
-			valueToParse = value.substring(2);
-		} else if (value.startsWith("x")) {
-			base = 16;
-			valueString = "0" + value;
-			valueToParse = value.substring(1);
-		} else if (value.startsWith("0b")) {
-			base = 2;
-			valueString = value;
-			valueToParse = value.substring(2);
-		} else if (value.startsWith("b")) {
-			base = 2;
-			valueString = "0" + value;
-			valueToParse = value.substring(1);
-		} else {
-			base = 10;
-			valueString = valueToParse = value;
-		}
+	/**
+	 * Creates an integer out of a string.
+	 * This infers the base of the integer using the string prefix:
+	 * - '#' or no prefix indicates a decimal number,
+	 * - '0x' or 'x' indicates a hexadecimal integer,
+	 * - '0b' or 'b' indicates a binary number.
+	 * 
+	 * @param valueString the string to parse
+	 */
+	public IntegerString(String valueString) {
+		this(valueString, 10);
+	}
+	/**
+	 * Creates an integer out of a string.
+	 * 
+	 * For prefixed integer strings (e.g., '0xDEADBEEF', '0b100000111110', '#123'),
+	 * the base is inferred.
+	 * 
+	 * If there is no prefix, then the base defaults to the defaultBase parameter.
+	 * 
+	 * @param valueString the string to parse
+	 * @param defaultBase the default base, if no prefix provided
+	 */
+	public IntegerString(String valueString, int defaultBase) {
+		int base = defaultBase;
+		boolean isNegative = valueString.startsWith("-");
 		
+		// Compute negative and base-override, if prefixes are present:
+		if (isNegative) {
+			valueString = valueString.substring(1);
+		}
+		if (valueString.startsWith("0x")) {
+			base = 16;
+			valueString = valueString.substring(2);
+		} else if (valueString.startsWith("x")) {
+			base = 16;
+			valueString = valueString.substring(1);
+		} else if (valueString.startsWith("0b")) {
+			base = 2;
+			valueString = valueString.substring(2);
+		} else if (valueString.startsWith("b")) {
+			base = 2;
+			valueString = valueString.substring(1);
+		} else if (valueString.startsWith("#")) {
+			base = 10;
+			valueString = valueString.substring(1);
+		}
+		// Check negative after prefix
+		if (!isNegative && valueString.startsWith("-")) {
+			isNegative = true;
+			valueString = valueString.substring(1);
+		}
+
+		// Parse value:
 		try {
-			this.value = (int)Long.parseLong(valueToParse, base);
+			this.value = (isNegative ? -1 : 1) * Integer.parseUnsignedInt(valueString, base);
 		} catch (NumberFormatException exc) {
 			throw new SimulationException(valueString + " is not a valid value of base " + base);
 		}
+		this.base = base;
+		// Readd prefixes (note this converts 'x' and 'b' to '0x' and '0b'):
+		String negPrefix = isNegative ? "-" : "";
+		String basePrefix = "";
+		if (base != defaultBase) {
+			basePrefix = switch (base) {
+				case 2 -> "0b";
+				case 10 -> "#";
+				case 16 -> "0x";
+				default -> "";
+			};
+		}
+		this.valueString = negPrefix + basePrefix + valueString;
 	}
 	
+	/**
+	 * Creates a new `IntegerString` from a base-10 integer.
+	 * @param value the integer
+	 */
 	public IntegerString(int value) {
 		this.value = value;
 		this.base = 10;
-		valueString = Integer.toString(value);
+		this.valueString = Integer.toString(value);
 	}
 	
 	public int getValue() {
@@ -54,27 +106,34 @@ public final class IntegerString {
 		return base;
 	}
 	
-	public String getValueString() {
-		return valueString;
+	/**
+	 * @return the string representation of this value (with an explicit prefix)
+	 */
+	public String prefixedString() {
+		return switch (base) {
+			case 2  -> String.format("0b%s", Integer.toString(value, 2));
+			case 10 -> String.format("#%d", value);
+			case 16 -> String.format("0x%X", value);
+			default -> Integer.toString(value);
+		};
 	}
-	
+
 	@Override
 	public boolean equals(Object o) {
 		if (o instanceof IntegerString) {
-			IntegerString other = (IntegerString)o;
-			return other.valueString.equals(this.valueString);
+			IntegerString other = (IntegerString) o;
+			return this.value == other.value && this.base == other.base;
 		}
-		
 		return false;
 	}
 	
 	@Override
 	public int hashCode() {
-		return valueString.hashCode();
+		return Objects.hash(value, base);
 	}
 	
 	@Override
 	public String toString() {
-		return valueString;
+		return this.valueString;
 	}
 }
